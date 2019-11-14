db0f6009382c3bf3fd449d849bb99eaca466bb63
Kevin Verschaeve <kevin.verschaeve@live.fr>
2019-11-14 19:24:12 +0100

    Allow to re run the script on each button click
    close #2

--- door.py
+++ door.py
@@ -3,21 +3,24 @@ import yaml
 from time import sleep
 from PigpioStepperMotor import StepperMotor, fullStepSequence
 from bluedot import BlueDot
+from signal import pause
 
-bd = BlueDot()
-bd.wait_for_press()
+def rotate():
+    for i in range(nb_steps):
+        motor.rotateClockwise()
+
+    sleep(config.get('wait_time', 1))
+
+    for i in range(nb_steps):
+        motor.rotateCounterClockwise()
 
 config = yaml.safe_load(open("config.yaml", 'r'))
 
 pi = pigpio.pi()
 motor = StepperMotor(pi, *config['pins'], sequence = fullStepSequence)
-
 nb_steps = config.get('nb_steps', 2048)
 
-for i in range(nb_steps):
-    motor.rotateClockwise()
+bd = BlueDot()
+bd.when_pressed = rotate
 
-sleep(config.get('wait_time', 1))
-
-for i in range(nb_steps):
-    motor.rotateCounterClockwise()
+pause()
